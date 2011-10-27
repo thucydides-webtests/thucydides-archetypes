e2a0cbf4913d1c00075e141ca56815a4c25339c8
John Ferguson Smart <john.smart@wakaleo.com>
2011-10-28 09:24:38 +1300

    Fixed some bugs in the archetype template

--- thucydides-easyb-archetype/src/main/resources/archetype-resources/src/test/java/steps/EndUserSteps.java
+++ thucydides-easyb-archetype/src/main/resources/archetype-resources/src/test/java/steps/EndUserSteps.java
@@ -8,7 +8,6 @@ import net.thucydides.core.annotations.Step;
 import net.thucydides.core.pages.Pages;
 import net.thucydides.core.steps.ScenarioSteps;
 
-import static ch.lambdaj.Lambda.join;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
