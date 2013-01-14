f15577f8269313534497efb6de21ff960bcda6f8
John Ferguson Smart <john.smart@wakaleo.com>
2013-01-14 06:52:37 +0000

    Updated to version 0.9.91

--- thucydides-jbehave-archetype/src/main/resources/archetype-resources/src/main/java/pages/DictionaryPage.java
+++ thucydides-jbehave-archetype/src/main/resources/archetype-resources/src/main/java/pages/DictionaryPage.java
@@ -3,18 +3,16 @@
 #set( $symbol_escape = '\' )
 package ${package}.pages;
 
-import ch.lambdaj.function.convert.Converter;
 import net.thucydides.core.annotations.DefaultUrl;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
+import net.thucydides.core.pages.PageObject;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import net.thucydides.core.pages.PageObject;
 
 import java.util.List;
 
-import static ch.lambdaj.Lambda.convert;
+import static ch.lambdaj.Lambda.extract;
+import static ch.lambdaj.Lambda.on;
 
 @DefaultUrl("http://en.wiktionary.org/wiki/Wiktionary:Main_Page")
 public class DictionaryPage extends PageObject {
@@ -24,6 +22,9 @@ public class DictionaryPage extends PageObject {
 
     @FindBy(name="go")
     private WebElement lookupButton;
+
+    @FindBy(css="ol li")
+    private List<WebElement> definitionList;
 
     public DictionaryPage(WebDriver driver) {
         super(driver);
@@ -37,20 +38,7 @@ public class DictionaryPage extends PageObject {
         $(lookupButton).click();
     }
 
-    public List<String> getDefinitions() {
-        WebElement definitionList = getDriver().findElement(By.tagName("ol"));
-        List<WebElement> results = definitionList.findElements(By.tagName("li"));
-
-        return convert(results, toStrings());
-    }
-
-    private Converter<WebElement, String> toStrings() {
-        return new Converter<WebElement, String>() {
-            public String convert(WebElement from) {
-                return from.getText();
-            }
-        };
+    public Iterable<String> getDefinitions() {
+        return extract(definitionList, on(WebElement.class).getText());
     }
 }
-
-
